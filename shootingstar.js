d35013a2cef4e9dfa4024fba75f37c0a3c81605c
Tommy Kasarcik <77713266+tgkasarcik@users.noreply.github.com>
2024-07-23 22:35:41 -0400

    Start working on shooting star class

---
+++ shootingstar.js
@@ -0,0 +1,16 @@
+class ShootingStar {
+    constructor (position, velocity) {
+        this.position = position;
+        this.velocity = velocity;
+    }
+
+    update() {
+
+    }
+
+    draw() {
+        push();
+
+        pop();
+    }
+}
