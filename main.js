99ab0af06ef7af3bbdf7706c330d97dad9406593
Tommy Kasarcik <77713266+tgkasarcik@users.noreply.github.com>
2024-10-12 18:06:34 -0400

    Exprimenting with different star effects, started adding content to page

--- main.js
+++ main.js
@@ -35,12 +35,18 @@ function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
+// function mouseWheel(event) {
+//   for (i = 0; i < stars.length; i++) {
+//     stars[i].scroll(event.delta);
+//   }
+// }
+
 function createShootingStars() {
   // All stars move upward for now
   let direction = createVector(0, -1);
   for (let i = 0; i < 100; i++) {
     let position = createVector(random(0, windowWidth), random(0, windowHeight));
-    let diameter = random(8, 32);
+    let diameter = random(8, 16);
     let velocity = 1 / diameter * 25;   // Inversely porportional to diamater - smaller move faster, bigger slower
     
 
--- shootingstar.js
+++ shootingstar.js
@@ -12,22 +12,37 @@ class ShootingStar {
         this.position.y += this.direction.y * this.velocity;
 
         // Move shooting stars that are close to the mouse toward it
-        let distToMouse = dist(this.position.x, this.position.y, mouseX, mouseY);
-        if (distToMouse < 250) {
-            let toMouse = createVector(mouseX - this.position.x, mouseY - this.position.y);
+        // let distToMouse = dist(this.position.x, this.position.y, mouseX, mouseY);
+        // if (distToMouse < 250) {
+        //     let toMouse = createVector(mouseX - this.position.x, mouseY - this.position.y);
             
-            // Move toward mouse with velocity inversely porportional to the dist from mouse
-            this.position.x += toMouse.x * 1 / distToMouse;
-            //this.position.y += toMouse.y * 1 / distToMouse;
+        //     // Move toward mouse with velocity inversely porportional to the dist from mouse
+        //     this.position.x += toMouse.x * 1 / distToMouse;
+        //     //this.position.y += toMouse.y * 1 / distToMouse;
 
-            print(distToMouse);
-        }
+        //     print(distToMouse);
+        // }
 
         // move to bottom of screen after going off top
         if (this.position.y <= 0) {
             this.position.y = windowHeight;
             this.position.x = random(0, windowWidth);
         }
+    }
+
+    scroll(deltaY) {
+
+        // this.position.y += this.direction.y * (this.velocity * deltaY/100);
+        this.position.y = lerp(this.position.y, this.direction.y * (this.velocity * deltaY/100), 0.02);
+
+
+        // move to bottom of screen after going off top
+        if (this.position.y <= 0) {
+            this.position.y = windowHeight;
+            this.position.x = random(0, windowWidth);
+        }
+
+        //TODO add similar logic for going off bottom of screen
     }
 
     draw() {
